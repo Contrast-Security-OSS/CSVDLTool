6c28e2a384146de019caa3a17d9335176aaf6362
turbou <turbou@i.softbank.jp>
2021-02-10 22:32:16 +0900

    コメントの特殊文字が文字化けしている問題に対応。

--- src/main/java/com/contrastsecurity/csvdltool/model/Note.java
+++ src/main/java/com/contrastsecurity/csvdltool/model/Note.java
@@ -1,8 +1,8 @@
 package com.contrastsecurity.csvdltool.model;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringEscapeUtils;
 
 public class Note {
     private String note;
@@ -13,7 +13,7 @@ public class Note {
     private List<Property> properties;
 
     public String getNote() {
-        return this.ncr(note);
+        return StringEscapeUtils.unescapeHtml4(note);
     }
 
     public void setNote(String note) {
@@ -63,38 +63,6 @@ public class Note {
     @Override
     public String toString() {
         return this.getNote();
-    }
-
-    private static final Pattern P = Pattern.compile("&#(x([0-9a-f]+)|([0-9]+));", Pattern.CASE_INSENSITIVE);
-
-    private boolean isHex(final String str) {
-        final char x = str.charAt(0);
-        return 'x' == x || 'X' == x;
-    }
-
-    public String ncr(final String str) {
-        final StringBuffer rtn = new StringBuffer();
-        final Matcher matcher = P.matcher(str);
-        while (matcher.find()) {
-            final String group = matcher.group(1);
-            int parseInt;
-            if (isHex(group)) {
-                parseInt = Integer.parseInt(group.substring(1), 16);
-            } else {
-                parseInt = Integer.parseInt(group, 10);
-            }
-
-            final char c;
-            if (0 != (0x0ffff & parseInt)) {
-                c = (char) parseInt;
-            } else {
-                c = '?';
-            }
-            matcher.appendReplacement(rtn, Character.toString(c));
-        }
-        matcher.appendTail(rtn);
-
-        return rtn.toString();
     }
 
 }

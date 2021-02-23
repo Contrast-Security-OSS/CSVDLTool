f5a42bdbc98883efb05d53f8e2221d7b585310e0
turbou <turbou@i.softbank.jp>
2021-02-23 15:50:21 +0900

    import.

---
+++ src/main/java/com/contrastsecurity/csvdltool/exception/NonApiException.java
@@ -0,0 +1,11 @@
+package com.contrastsecurity.csvdltool.exception;
+
+public class NonApiException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public NonApiException(String message) {
+        super(message);
+    }
+
+}

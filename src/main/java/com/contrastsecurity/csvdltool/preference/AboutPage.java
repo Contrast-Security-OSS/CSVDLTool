a91e4d022765a0f207460c336d167389003e98ed
turbou <turbou@i.softbank.jp>
2021-05-24 22:13:58 +0900

    1.3.0up.

--- src/main/java/com/contrastsecurity/csvdltool/preference/AboutPage.java
+++ src/main/java/com/contrastsecurity/csvdltool/preference/AboutPage.java
@@ -62,7 +62,7 @@ public class AboutPage extends PreferencePage {
         Label versionValueLbl = new Label(appGrp, SWT.NONE);
         GridData versionValueLblGrDt = new GridData();
         versionValueLbl.setLayoutData(versionValueLblGrDt);
-        versionValueLbl.setText("1.2.4");
+        versionValueLbl.setText("1.3.0");
 
         Label copyrightLbl = new Label(appGrp, SWT.NONE);
         GridData copyrightLblGrDt = new GridData();

2d1d07b069db8c203491c00e6c2153ed69480d49
elmore-mk2 <elmore.mk2@gmail.com>
2021-06-15 19:51:15 +0900

    暗号化のサンプルを追加

--- src/main/java/com/contrastsecurity/csvdltool/CSVDLEncryptor.java
+++ src/main/java/com/contrastsecurity/csvdltool/CSVDLEncryptor.java
@@ -1,7 +1,11 @@
 package com.contrastsecurity.csvdltool;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
+import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
 import org.jasypt.util.text.BasicTextEncryptor;
+
+import java.io.FileNotFoundException;
 
 public class CSVDLEncryptor {
 
@@ -9,17 +13,34 @@ public class CSVDLEncryptor {
     private static String ENV_KEY = "CSVDEV_TOOL_PASS";
 
     public static String encrypt(String planText) {
-        CSVDLEncryptor.encryptor.setPassword(getPassword());
+        try {
+            CSVDLEncryptor.encryptor.setPassword(getPassword());
+        } catch (FileNotFoundException fne) {
+            return planText;
+        }
         return CSVDLEncryptor.encryptor.encrypt(planText);
     }
 
     public static String decrypt(String encryptedText) {
-        CSVDLEncryptor.encryptor.setPassword(getPassword());
-        return CSVDLEncryptor.encryptor.decrypt(encryptedText);
+        // .envファイルがない、復号できない場合はそのままの値を返す。
+        try {
+            CSVDLEncryptor.encryptor.setPassword(getPassword());
+        } catch (FileNotFoundException fne) {
+            return encryptedText;
+        }
+        try {
+            return CSVDLEncryptor.encryptor.decrypt(encryptedText);
+        } catch (EncryptionOperationNotPossibleException encryptionOperationNotPossibleException) {
+            return encryptedText;
+        }
     }
 
-    private static String getPassword() {
-        Dotenv dotenv = Dotenv.load();
-        return dotenv.get(ENV_KEY);
+    private static String getPassword() throws FileNotFoundException {
+        try {
+            Dotenv dotenv = Dotenv.load();
+            return dotenv.get(ENV_KEY);
+        } catch (DotenvException de) {
+            throw new FileNotFoundException();
+        }
     }
 }

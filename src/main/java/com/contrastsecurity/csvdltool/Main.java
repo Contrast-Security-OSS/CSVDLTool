22491168b59e225dcaf1d6b3e6f9c368868de09b
turbou <turbou@i.softbank.jp>
2022-09-05 01:25:58 +0900

    bug fixed.

--- src/main/java/com/contrastsecurity/csvdltool/Main.java
+++ src/main/java/com/contrastsecurity/csvdltool/Main.java
@@ -1177,6 +1177,7 @@ public class Main implements PropertyChangeListener {
         attackLoadBtn.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent event) {
+                filteredAttackEvents.clear();
                 attackTable.clearAll();
                 attackTable.removeAll();
                 Date[] frToDate = getFrToDetectedDate();

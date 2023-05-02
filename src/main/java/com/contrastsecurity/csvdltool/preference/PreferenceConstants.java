0e3b1ea47bac60cd06654c604ca3fbe277346fc8
turbou <taka.shiozaki@contrastsecurity.com>
2023-05-03 01:06:14 +0900

    localization.

--- src/main/java/com/contrastsecurity/csvdltool/preference/PreferenceConstants.java
+++ src/main/java/com/contrastsecurity/csvdltool/preference/PreferenceConstants.java
@@ -24,64 +24,64 @@
 package com.contrastsecurity.csvdltool.preference;
 
 public class PreferenceConstants {
-    public static final String CONTRAST_URL = new String("com.contrastsecurity.csvdltool.contrast_url");
-    public static final String USERNAME = new String("com.contrastsecurity.csvdltool.username");
-    public static final String SERVICE_KEY = new String("com.contrastsecurity.csvdltool.service_key");
-    public static final String PASSWORD = new String("com.contrastsecurity.csvdltool.password");
-    public static final String BASIC_AUTH_STATUS = new String("com.contrastsecurity.csvdltool.basic_auth_status");
-    public static final String PASS_TYPE = new String("com.contrastsecurity.csvdltool.pass_type");
-    public static final String XSRF_TOKEN = new String("com.contrastsecurity.csvdltool.xsrf_token");
-    public static final String TARGET_ORGS = new String("com.contrastsecurity.csvdltool.target_orgs");
-    public static final String TSV_STATUS = new String("com.contrastsecurity.csvdltool.tsv_status");
-    public static final String AUTH_RETRY_MAX = new String("com.contrastsecurity.csvdltool.auth_retry_max");
+    public static final String CONTRAST_URL = new String("com.contrastsecurity.csvdltool.contrast_url"); //$NON-NLS-1$
+    public static final String USERNAME = new String("com.contrastsecurity.csvdltool.username"); //$NON-NLS-1$
+    public static final String SERVICE_KEY = new String("com.contrastsecurity.csvdltool.service_key"); //$NON-NLS-1$
+    public static final String PASSWORD = new String("com.contrastsecurity.csvdltool.password"); //$NON-NLS-1$
+    public static final String BASIC_AUTH_STATUS = new String("com.contrastsecurity.csvdltool.basic_auth_status"); //$NON-NLS-1$
+    public static final String PASS_TYPE = new String("com.contrastsecurity.csvdltool.pass_type"); //$NON-NLS-1$
+    public static final String XSRF_TOKEN = new String("com.contrastsecurity.csvdltool.xsrf_token"); //$NON-NLS-1$
+    public static final String TARGET_ORGS = new String("com.contrastsecurity.csvdltool.target_orgs"); //$NON-NLS-1$
+    public static final String TSV_STATUS = new String("com.contrastsecurity.csvdltool.tsv_status"); //$NON-NLS-1$
+    public static final String AUTH_RETRY_MAX = new String("com.contrastsecurity.csvdltool.auth_retry_max"); //$NON-NLS-1$
 
-    public static final String PROXY_YUKO = new String("com.contrastsecurity.csvdltool.proxy_yuko");
-    public static final String PROXY_HOST = new String("com.contrastsecurity.csvdltool.proxy_host");
-    public static final String PROXY_PORT = new String("com.contrastsecurity.csvdltool.proxy_port");
-    public static final String PROXY_AUTH = new String("com.contrastsecurity.csvdltool.proxy_auth");
-    public static final String PROXY_USER = new String("com.contrastsecurity.csvdltool.proxy_user");
-    public static final String PROXY_PASS = new String("com.contrastsecurity.csvdltool.proxy_pass");
-    public static final String PROXY_TMP_USER = new String("com.contrastsecurity.csvdltool.proxy_tmp_user");
-    public static final String PROXY_TMP_PASS = new String("com.contrastsecurity.csvdltool.proxy_tmp_pass");
-    public static final String IGNORE_SSLCERT_CHECK = new String("com.contrastsecurity.csvdltool.ignore_sslcert_check");
-    public static final String CONNECTION_TIMEOUT = new String("com.contrastsecurity.csvdltool.connection_timeout");
-    public static final String SOCKET_TIMEOUT = new String("com.contrastsecurity.csvdltool.socket_timeout");
-    public static final String AUTO_RELOGIN_INTERVAL = new String("com.contrastsecurity.csvdltool.auto_relogin_interval");
+    public static final String PROXY_YUKO = new String("com.contrastsecurity.csvdltool.proxy_yuko"); //$NON-NLS-1$
+    public static final String PROXY_HOST = new String("com.contrastsecurity.csvdltool.proxy_host"); //$NON-NLS-1$
+    public static final String PROXY_PORT = new String("com.contrastsecurity.csvdltool.proxy_port"); //$NON-NLS-1$
+    public static final String PROXY_AUTH = new String("com.contrastsecurity.csvdltool.proxy_auth"); //$NON-NLS-1$
+    public static final String PROXY_USER = new String("com.contrastsecurity.csvdltool.proxy_user"); //$NON-NLS-1$
+    public static final String PROXY_PASS = new String("com.contrastsecurity.csvdltool.proxy_pass"); //$NON-NLS-1$
+    public static final String PROXY_TMP_USER = new String("com.contrastsecurity.csvdltool.proxy_tmp_user"); //$NON-NLS-1$
+    public static final String PROXY_TMP_PASS = new String("com.contrastsecurity.csvdltool.proxy_tmp_pass"); //$NON-NLS-1$
+    public static final String IGNORE_SSLCERT_CHECK = new String("com.contrastsecurity.csvdltool.ignore_sslcert_check"); //$NON-NLS-1$
+    public static final String CONNECTION_TIMEOUT = new String("com.contrastsecurity.csvdltool.connection_timeout"); //$NON-NLS-1$
+    public static final String SOCKET_TIMEOUT = new String("com.contrastsecurity.csvdltool.socket_timeout"); //$NON-NLS-1$
+    public static final String AUTO_RELOGIN_INTERVAL = new String("com.contrastsecurity.csvdltool.auto_relogin_interval"); //$NON-NLS-1$
 
-    public static final String SLEEP_VUL = new String("com.contrastsecurity.csvdltool.sleep_vul");
+    public static final String SLEEP_VUL = new String("com.contrastsecurity.csvdltool.sleep_vul"); //$NON-NLS-1$
 
-    public static final String CSV_OUT_HEADER_VUL = new String("com.contrastsecurity.csvdltool.csv_out_header_vul");
-    public static final String CSV_COLUMN_VUL = new String("com.contrastsecurity.csvdltool.csv_out_column_vul");
-    public static final String CSV_FILE_FORMAT_VUL = new String("com.contrastsecurity.csvdltool.csv_file_format_vul");
+    public static final String CSV_OUT_HEADER_VUL = new String("com.contrastsecurity.csvdltool.csv_out_header_vul"); //$NON-NLS-1$
+    public static final String CSV_COLUMN_VUL = new String("com.contrastsecurity.csvdltool.csv_out_column_vul"); //$NON-NLS-1$
+    public static final String CSV_FILE_FORMAT_VUL = new String("com.contrastsecurity.csvdltool.csv_file_format_vul"); //$NON-NLS-1$
 
-    public static final String SLEEP_LIB = new String("com.contrastsecurity.csvdltool.sleep_lib");
+    public static final String SLEEP_LIB = new String("com.contrastsecurity.csvdltool.sleep_lib"); //$NON-NLS-1$
 
-    public static final String CSV_OUT_HEADER_LIB = new String("com.contrastsecurity.csvdltool.csv_out_header_lib");
-    public static final String CSV_COLUMN_LIB = new String("com.contrastsecurity.csvdltool.csv_out_column_lib");
-    public static final String CSV_FILE_FORMAT_LIB = new String("com.contrastsecurity.csvdltool.csv_file_format_lib");
+    public static final String CSV_OUT_HEADER_LIB = new String("com.contrastsecurity.csvdltool.csv_out_header_lib"); //$NON-NLS-1$
+    public static final String CSV_COLUMN_LIB = new String("com.contrastsecurity.csvdltool.csv_out_column_lib"); //$NON-NLS-1$
+    public static final String CSV_FILE_FORMAT_LIB = new String("com.contrastsecurity.csvdltool.csv_file_format_lib"); //$NON-NLS-1$
 
-    public static final String ATTACK_RANGE_DAYTIME = new String("com.contrastsecurity.csvdltool.attack_range_daytime");
-    public static final String ATTACK_RANGE_NIGHTTIME = new String("com.contrastsecurity.csvdltool.attack_range_nighttime");
-    public static final String ATTACK_START_WEEKDAY = new String("com.contrastsecurity.csvdltool.attack_start_weekday");
-    public static final String ATTACK_DETECTED_DATE_FILTER = new String("com.contrastsecurity.csvdltool.attack_detected_date_filter");
-    public static final String CSV_OUT_HEADER_ATTACKEVENT = new String("com.contrastsecurity.csvdltool.csv_out_header_attackevent");
-    public static final String CSV_COLUMN_ATTACKEVENT = new String("com.contrastsecurity.csvdltool.csv_out_column_attackevent");
-    public static final String CSV_FILE_FORMAT_ATTACKEVENT = new String("com.contrastsecurity.csvdltool.csv_file_format_attackevent");
+    public static final String ATTACK_RANGE_DAYTIME = new String("com.contrastsecurity.csvdltool.attack_range_daytime"); //$NON-NLS-1$
+    public static final String ATTACK_RANGE_NIGHTTIME = new String("com.contrastsecurity.csvdltool.attack_range_nighttime"); //$NON-NLS-1$
+    public static final String ATTACK_START_WEEKDAY = new String("com.contrastsecurity.csvdltool.attack_start_weekday"); //$NON-NLS-1$
+    public static final String ATTACK_DETECTED_DATE_FILTER = new String("com.contrastsecurity.csvdltool.attack_detected_date_filter"); //$NON-NLS-1$
+    public static final String CSV_OUT_HEADER_ATTACKEVENT = new String("com.contrastsecurity.csvdltool.csv_out_header_attackevent"); //$NON-NLS-1$
+    public static final String CSV_COLUMN_ATTACKEVENT = new String("com.contrastsecurity.csvdltool.csv_out_column_attackevent"); //$NON-NLS-1$
+    public static final String CSV_FILE_FORMAT_ATTACKEVENT = new String("com.contrastsecurity.csvdltool.csv_file_format_attackevent"); //$NON-NLS-1$
 
-    public static final String CSV_OUT_HEADER_SERVER = new String("com.contrastsecurity.csvdltool.csv_out_header_server");
-    public static final String CSV_COLUMN_SERVER = new String("com.contrastsecurity.csvdltool.csv_out_column_server");
-    public static final String CSV_FILE_FORMAT_SERVER = new String("com.contrastsecurity.csvdltool.csv_file_format_server");
+    public static final String CSV_OUT_HEADER_SERVER = new String("com.contrastsecurity.csvdltool.csv_out_header_server"); //$NON-NLS-1$
+    public static final String CSV_COLUMN_SERVER = new String("com.contrastsecurity.csvdltool.csv_out_column_server"); //$NON-NLS-1$
+    public static final String CSV_FILE_FORMAT_SERVER = new String("com.contrastsecurity.csvdltool.csv_file_format_server"); //$NON-NLS-1$
 
-    public static final String VUL_ONLY_PARENT_APP = new String("com.contrastsecurity.csvdltool.only_parent_app_check");
-    public static final String VUL_ONLY_CURVUL_EXP = new String("com.contrastsecurity.csvdltool.only_curvul_exp_check");
-    public static final String INCLUDE_DESCRIPTION = new String("com.contrastsecurity.csvdltool.include_description");
-    public static final String INCLUDE_STACKTRACE = new String("com.contrastsecurity.csvdltool.include_stacktrace");
+    public static final String VUL_ONLY_PARENT_APP = new String("com.contrastsecurity.csvdltool.only_parent_app_check"); //$NON-NLS-1$
+    public static final String VUL_ONLY_CURVUL_EXP = new String("com.contrastsecurity.csvdltool.only_curvul_exp_check"); //$NON-NLS-1$
+    public static final String INCLUDE_DESCRIPTION = new String("com.contrastsecurity.csvdltool.include_description"); //$NON-NLS-1$
+    public static final String INCLUDE_STACKTRACE = new String("com.contrastsecurity.csvdltool.include_stacktrace"); //$NON-NLS-1$
 
-    public static final String ONLY_HAS_CVE = new String("com.contrastsecurity.csvdltool.only_has_cve");
-    public static final String INCLUDE_CVE_DETAIL = new String("com.contrastsecurity.csvdltool.include_cve_detail");
+    public static final String ONLY_HAS_CVE = new String("com.contrastsecurity.csvdltool.only_has_cve"); //$NON-NLS-1$
+    public static final String INCLUDE_CVE_DETAIL = new String("com.contrastsecurity.csvdltool.include_cve_detail"); //$NON-NLS-1$
 
-    public static final String OPENED_MAIN_TAB_IDX = new String("com.contrastsecurity.csvdltool.opened_main_tab_idx");
-    public static final String OPENED_SUB_TAB_IDX = new String("com.contrastsecurity.csvdltool.opened_sub_tab_idx");
-    public static final String MEM_WIDTH = new String("com.contrastsecurity.csvdltool.mem_width");
-    public static final String MEM_HEIGHT = new String("com.contrastsecurity.csvdltool.mem_height");
+    public static final String OPENED_MAIN_TAB_IDX = new String("com.contrastsecurity.csvdltool.opened_main_tab_idx"); //$NON-NLS-1$
+    public static final String OPENED_SUB_TAB_IDX = new String("com.contrastsecurity.csvdltool.opened_sub_tab_idx"); //$NON-NLS-1$
+    public static final String MEM_WIDTH = new String("com.contrastsecurity.csvdltool.mem_width"); //$NON-NLS-1$
+    public static final String MEM_HEIGHT = new String("com.contrastsecurity.csvdltool.mem_height"); //$NON-NLS-1$
 }

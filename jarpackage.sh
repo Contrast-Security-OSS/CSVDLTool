1db94260879527b2acd596c6c5b3120801c2c45d
turbou <turbou@i.softbank.jp>
2023-10-06 22:39:38 +0900

    update.

--- jarpackage.sh
+++ jarpackage.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-JAVA_HOME="C:\Program Files\Java\jdk1.8.0_202"
-BUNDLE_JRE_HOME="C:\Users\turbou\Downloads\jre-8u202-macosx-x64.tar\jre1.8.0_202.jre"
+JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-1.8.jdk/Contents/Home"
+BUNDLE_JRE_HOME="/Users/turbou/Downloads/jre1.8.0_202.jre"
 APP_NAME="CSVDLTool"
 APP_VERSION="2.1.0"
 APP_DIR_NAME="${APP_NAME}_${APP_VERSION}.app"
 
 javapackager \
-  -deploy -Bruntime=${BUNDLE_JRE_HOME} \
+  -deploy -Bruntime= \
   -native image \
   -srcdir ./build/libs \
   -srcfiles ${APP_NAME}-${APP_VERSION}.jar \
@@ -22,8 +22,7 @@ javapackager \
   -vendor "Contrast Security Japan G.K." \
   -Bicon=src/main/resources/csvdltool.icns \
   -Bmac.CFBundleVersion=${APP_VERSION} \
-  -nosign \
-  -v
+  -nosign
 
 echo ""
 echo "If that succeeded, it created \"build/libs/bundles/${APP_DIR_NAME}\""

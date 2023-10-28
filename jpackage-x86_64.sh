bc2a063dae8b9a9643ea6ef21780077a3d7d1785
turbou <turbou@i.softbank.jp>
2023-10-29 00:00:15 +0900

    update.

--- jpackage-x86_64.sh
+++ jpackage-x86_64.sh
@@ -25,8 +25,8 @@ jpackage \
 smctl-mac-x64 sign \
   --keypair-alias ${SM_KEY_PAIR} \
   --config-file /Users/turbou/digicert/pkcs11properties.cfg \
-  --input build/${APP_NAME}-${APP_VERSION}-aarch64.app/
+  --input build/${APP_NAME}-${APP_VERSION}-x86_64.app/
 
-7z a build/${APP_NAME}-${APP_VERSION}-aarch64.cli7z build/${APP_NAME}-${APP_VERSION}-aarch64.app/
+cd build && 7z a ${APP_NAME}-${APP_VERSION}-aarch64.cli7z ${APP_NAME}-${APP_VERSION}-x86_64.app/
 
 exit 0
